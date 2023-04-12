85a623ed356e709775bf33f4794ccd674464862c
el-aminino <aminmze81@gmail.com>
2023-04-12 22:24:16 +0330

    something easily updated

--- src/header.php
+++ src/header.php
@@ -12,7 +12,8 @@
         <?php 
             include 'navbar.php';
         ?>
-        <?php /* 
+        <?php 
+        /* 
         <h1>Amin Mahdizade</h1>
         <h3>trying to make a CMS using php</h3>
         <hr>
